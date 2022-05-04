13d3afdcff0050da14b1c5e3a86af60c46a5a11c
tingguang.yan <522125842@qq.com>
2022-05-04 18:28:36 +0800

    add xstate demo

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,31 +1,90 @@
+import { assign, createMachine, interpret } from 'xstate'
 import data from '@ytg/core';
 
 type DemoData = {
   userId: number;
   user: string;
   error: null | Error;
+  data?: string
 };
 
-const normalTask = () => {
-  return new Promise<DemoData>((resolve, reject) => {
-    setTimeout(() => {
-      if (Math.random() > 0.5) {
-        resolve({
-          userId: Math.random(),
-          user: 'hello',
-          error: null,
-        });
-      } else {
-        reject({
-          userId: 0,
-          user: '',
-          error: new Error('test'),
-        });
+const normalTask = () => new Promise<DemoData>((resolve, reject) => {
+  setTimeout(() => {
+    if (Math.random() > 0.5) {
+      resolve({
+        userId: Math.random(),
+        user: 'hello',
+        error: null,
+      });
+    } else {
+      reject({
+        userId: 0,
+        user: '',
+        error: new Error('test'),
+      });
+    }
+  }, 1000);
+});
+
+const fetchCuteAnimals = () => {
+  return normalTask;
+}
+
+const toggleMachine = createMachine({
+  id: 'toggle',
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        TOGGLE: 'active'
       }
-    }, 1000);
-  });
-};
+    },
+    active: {
+      on: {
+        TOGGLE: 'inactive'
+      }
+    }
+  }
+})
 
-export { normalTask };
+const toggleMachine1 = createMachine({
+  id: 'user',
+  initial: 'idle',
+  context: {} as DemoData,
+  states: {
+    idle: {
+      on: {
+        FETCH: { target: 'loading' }
+      }
+    },
+    loading: {
+      invoke: {
+        id: 'getUser',
+        src: fetchCuteAnimals,
+        onDone: {
+          target: 'success',
+          actions: assign({ user: (_context, event) => event.data })
+        },
+        onError: {
+          target: 'failure',
+          actions: assign({ error: (_context, event) => event.data.error })
+        }
+      }
+    },
+    success: { type: 'final' },
+    failure: {
+      on: {
+        RETRY: { target: 'loading' }
+      }
+    }
+  }
+})
+
+const toggleService = interpret(toggleMachine).start()
+toggleService.onTransition(state => {
+  console.log('value', state.value)
+})
+
+export { normalTask, fetchCuteAnimals };
 
 console.log('data', data + '🐱');
